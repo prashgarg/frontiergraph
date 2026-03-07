935bff7cd67d262c40c865fe541bf8a8fff85cf7
Prashant Garg <prashant.garg@imperial.ac.uk>
2026-03-07 12:47:36 +0000

    Wire homepage to live beta app

--- site/config.js
+++ site/config.js
@@ -1,5 +1,4 @@
 window.RANKER_CONFIG = {
-  appUrl: "https://economics-opportunity-ranker-beta-placeholder.run.app",
+  appUrl: "https://economics-opportunity-ranker-beta-1058669339361.us-central1.run.app",
   docsUrl: "https://github.com/prashgarg/frontiergraph",
-  paperUrl: "https://frontiergraph.com/method",
 };
